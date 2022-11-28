73ec95135386b45133d2fb3dcebf5866682876e4
elisabeth <elisabeth.szlapa@mail.utoronto.ca>
2022-11-27 23:58:30 -0500

    added highscores class

---
+++ BoggleGame/Highscores/Highscores.java
@@ -0,0 +1,11 @@
+package Highscores;
+import java.util.*;
+public class Highscores {
+    /**dictionary
+     *
+     */
+    private Map<String, Integer> pointHighscores = new HashMap<>(10);
+    private Map<String, Integer> ratioHighscores = new HashMap<>(10);
+    public void getHighscores() {
+    }
+}
