c7d58127b188c7327749b2546117d4a329cee57d
elisabeth <elisabeth.szlapa@mail.utoronto.ca>
2022-11-30 01:06:20 -0500

    accessibility

--- BoggleGame/Highscores/Highscores.java
+++ BoggleGame/Highscores/Highscores.java
@@ -16,7 +16,6 @@ public class Highscores {
     private Map<String, Integer> pointHighscores = new HashMap<>(10);
     private Map<String, Integer> ratioHighscores = new HashMap<>(10);
     private Map<String, Integer> allUsersHighscores = new HashMap<>();
-
     public void scoreComparer(BoggleStats game) {
         int currScore = game.getScore();
         String playerName = ""; //relies on the as of yet not implemented user class!
@@ -51,12 +50,62 @@ public class Highscores {
         System.out.println("Point based, with the 10 highest scores for points won,");
         System.out.println("Ratio based, with the 10 highest scores for the highest ratio of words found to words possible on the board,");
         System.out.println("and Personal bests, the best score that every user who's played has received.");
+        System.out.println("0111001110101100111010111010000110100001111010100100011100101010011010100010010100\n" +
+                "1010000101000100101101100111001111010010000111011000100100101100101010010011101100\n" +
+                "0000001110110000101000000010001000000010001010001000001000001000001010000000001000");
     }
     public void scoreInterface() {
         System.out.println("To see point scores, press 1. To see ratio scores, press 2.");
         System.out.println("To see personal bests, press 3. And to have the boards explained again, hit 0.");
+        System.out.println("Want to exit the scoreboard system? Hit 4.");
         String choice = scanner.nextLine();
-        if(choice == "1"){
+        while(true) {
+            if (choice == "1") {
+                formatted(pointHighscores);
+                break;
+            }
+
+            if (choice == "2") {
+                formatted(ratioHighscores);
+                break;
+            }
+            if (choice == "3") {
+                formatted(allUsersHighscores);
+                break;
+            }
+            if (choice == "0") {
+                scoreInterface();
+                break;
+            }
+            if (choice == "6"){
+            }
+            if (choice == "4") {
+                break;
+            }
+            while(choice != "1" || choice != "2" || choice != "3" || choice != "4" || choice != "0"){
+                System.out.println("That's not one of the numbers! Press 0 to have the options explained again. Changed your mind about seeing the scores? Hit 4 to exit.");
+                if (choice == "4"){
+
+                }
+            }
+        }
+    }
+    public void formatted(Map<String, Integer> scoreboard){
+        int i = 1;
+        for (String key : scoreboard.keySet()){
+            System.out.println(i+". " + key+", "+scoreboard.get(key));
+            i++;
+        }
+    }
+    public void brailleMode(){
+
+    }
+
+    public void formattedBraille(Map<String, Integer> scoreboard){
+        int i = 1;
+        for (String key : scoreboard.keySet()){
+            System.out.println(i+". " + key+", "+scoreboard.get(key));
+            i++;
         }
     }
 }
